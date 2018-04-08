23cfff066e45ec3fceaefe11f8174cebd1ffaf96
Karina <karina_cheshko@mail.ru>
2018-04-08 17:42:58 +0300

    before global change

--- UI/dom.js
+++ UI/dom.js
@@ -9,12 +9,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.getElementById('load-more-id').style.display = "none";
         document.getElementById('loading-state').style.display = "table";
 
-        var posts = Module.getPhotoPosts(10, 10);
+        let posts = Module.getPhotoPosts(10, 10);
 
         document.getElementById('load-more-id').style.display = "table";
         document.getElementById('loading-state').style.display = "none";
     };
-
 
     //эффект кнопки для добавления фото
     document.getElementById('add-post').onclick = function () {
@@ -23,8 +22,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     function scrollTo(element, to, duration) {
         if (duration < 0) return;
-        var difference = to - element.scrollTop;
-        var perTick = difference / duration * 10;
+        let difference = to - element.scrollTop;
+        let perTick = difference / duration * 10;
 
         setTimeout(function () {
             element.scrollTop = element.scrollTop + perTick;
@@ -35,6 +34,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
     DomModule.displayPhotoPosts(Module.getPhotoPosts());
     DomModule.initUser();
 
+
+
+
+    let tr = document.getElementsByClassName("add-button");
+
+    tr[0].onclick = function () {
+        elem = document.getElementsByClassName('add-post')[0];
+        if (!elem.classList.contains('active')) {
+            elem.classList.add('active')
+        } else {
+            elem.classList.remove('active');
+
+        }
+    };
+
+
+
+
 }());
-
-
